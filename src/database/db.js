f140c9c514eae9839a780bed2e2828a159016975
Ana Clara Lélis <anaa.leliis@gmail.com>
2020-06-06 20:39:11 -0300

    criando a tabela place

--- src/database/db.js
+++ src/database/db.js
@@ -3,3 +3,32 @@ const sqlite3 = require("sqlite3").verbose() //ver mensagem no  meu terminal do 
 
 //Criar o objeto que irá fazer operações no banco de dados  
 const db = new sqlite3.Database("./src/database/database.db") //new cria um objeto caminho para criacao do banco
+
+
+//utilizar o objeto de banco de dados , para as nossas operaçoes
+db.serialize(function(){ //serializa vai rodar uma sequencia de codigo 
+   //com comando SQL eu vou :
+  
+  //1 criar uma tabela no banco de dados
+   db.run(`
+    CREATE TABLE IF NOT EXISTS places (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      image TEXT,
+      address TEXT,
+      address2 TEXT,
+      state TEXT,
+      city TEXT,
+      items TEXT
+
+    );
+  
+  `)
+
+  //2 inserir dados na tabela
+
+
+  //3 Consultar os dados da tabela
+
+
+  //4 Deletar dado de tabela
+}) 
