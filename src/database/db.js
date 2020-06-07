9b746b12e7a613f36e5ec07f6b2085aa2136e92e
Ana Clara Lélis <anaa.leliis@gmail.com>
2020-06-06 21:04:11 -0300

    comando para inserir dados na tabela places

--- src/database/db.js
+++ src/database/db.js
@@ -6,14 +6,15 @@ const db = new sqlite3.Database("./src/database/database.db") //new cria um obje
 
 
 //utilizar o objeto de banco de dados , para as nossas operaçoes
-db.serialize(function(){ //serializa vai rodar uma sequencia de codigo 
-   //com comando SQL eu vou :
-  
+db.serialize(function () { //serializa vai rodar uma sequencia de codigo 
+  //com comando SQL eu vou :
+
   //1 criar uma tabela no banco de dados
-   db.run(`
+  db.run(`
     CREATE TABLE IF NOT EXISTS places (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       image TEXT,
+      name TEXT,
       address TEXT,
       address2 TEXT,
       state TEXT,
@@ -24,7 +25,44 @@ db.serialize(function(){ //serializa vai rodar uma sequencia de codigo
   
   `)
 
-  //2 inserir dados na tabela
+  //2 inserir dados na tabela //inserir esses campos image
+  const query = ` 
+    INSERT INTO places ( 
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+      
+    ) VALUES (?,?,?,?,?,?,?);
+
+  `
+  const values = [
+    "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60",
+    "Colectoria",
+    "Guilherme Gembalia, Jardim América",
+    "N°260",
+    "Santa Catarina",
+    "Rio do Sul",
+    "Resíduos Eletrônicos, Lâmpadas"
+  ]
+
+  function afterInsertData(err){
+    if(err){
+      return console.log(err)
+    }
+
+    console.log("Cadastrado com Sucesso")
+    console.log(this) //esta referenciando a resposta do run
+  }
+
+  db.run(query, values, afterInsertData ) 
+  //callback a aplicação continua acontecendo , 
+  //quando tivesse a resposta do que foi pedido
+  //executa a função 
+  //Depois que a aplicação query , values for executada ai o metodo vai ser executado
 
 
   //3 Consultar os dados da tabela
